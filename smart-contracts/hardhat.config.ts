0923c01943d31ad6a09a857bac5151b1dc775ba2
MubarekHussen <mubahussen2014@gmail.com>
2024-02-10 02:30:48 +0300

    feat: configuring to deploy on alchemy

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,11 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+if (!ALCHEMY_API_KEY || !PRIVATE_KEY) {
+  throw new Error('Please set your Alchemy API key and private key in a .env file');
+}
 
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.19",
+        version: "0.8.20",
         settings: {
           optimizer: {
             enabled: true,
@@ -17,6 +27,12 @@ const config: HardhatUserConfig = {
         version: "0.4.20"
       }
     ]
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
   }
 };
 
